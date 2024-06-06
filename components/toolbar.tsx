6f0024973c5cf09dcb66e124b252a0f861e3d7d5
SebastianEscudero <sebastin.escudero@uc.cl>
2024-06-06 09:23:25 -0400

    slight fixes to colors

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -225,22 +225,22 @@ export const Toolbar = ({
             onValueChange={handleStrokeSizeChange}
           />
           <div className="grid grid-cols-4 gap-[2px]" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-            <ColorButton color={{ r: 0, g: 0, b: 0, a: 0 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 255, g: 255, b: 255, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 29, g: 29, b: 29, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 159, g: 168, b: 178, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 255, g: 240, b: 0, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 255, g: 255, b: 0, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 225, g: 133, b: 244, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 174, g: 62, b: 201, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 68, g: 101, b: 233, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 75, g: 161, b: 241, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 255, g: 165, b: 0, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 225, g: 105, b: 25, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 7, g: 147, b: 104, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 77, g: 176, b: 94, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 248, g: 119, b: 119, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
-            <ColorButton color={{ r: 224, g: 49, b: 49, a: 1 }}  onClick={onPathColorChange} pathColor={pathColor}/>
+            <ColorButton color={{ r: 0, g: 0, b: 0, a: 0 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ r: 255, g: 255, b: 255, a: 1 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ r: 29, g: 29, b: 29, a: 1 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ r: 159, g: 168, b: 178, a: 1 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ r: 255, g: 240, b: 0, a: 1 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ r: 255, g: 255, b: 0, a: 1 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ r: 225, g: 133, b: 244, a: 1 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ r: 174, g: 62, b: 201, a: 1 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ r: 68, g: 101, b: 233, a: 1 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ r: 75, g: 161, b: 241, a: 1 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ r: 255, g: 165, b: 0, a: 1 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ a: 1, b: 42, g: 142, r: 252 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ r: 7, g: 147, b: 104, a: 1 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ a: 1, b: 99, g: 202, r: 68 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ r: 248, g: 119, b: 119, a: 1 }} onClick={onPathColorChange} pathColor={pathColor} />
+            <ColorButton color={{ r: 224, g: 49, b: 49, a: 1 }} onClick={onPathColorChange} pathColor={pathColor} />
           </div>
         </div>
       }
